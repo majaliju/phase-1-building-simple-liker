f0526f2a754567afce30b6531846f6791d4322ae
majaliju <majalijunyc@gmail.com>
2022-03-25 23:18:55 -0400

    completed

--- main.js
+++ main.js
@@ -2,7 +2,54 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// turn our HTML collection of hearts into an array, accessing each heart-class of 'like-glyph'
+let heartsNodeArray = [...document.getElementsByClassName('like-glyph')];
+
+// define the modal & add the hidden class immediately to #modal
+const modal = document.getElementById("modal")
+modal.classList.add("hidden")
+
+// define the modal paragraph contents
+let modalParagraph = document.getElementById('modal-message');
+
+// express the catch for error message
+let callServerAndCatch = (event) => {
+  mimicServerCall()
+  .then(() => handleResponse(event))
+  .catch ((error) => handleError(error))
+}
+
+// express the modal and display the error message, once total per event(s)
+let handleError = (errorMessage) => {
+  modal.classList.remove('hidden')
+  modalParagraph.innerText = errorMessage
+  setTimeout(() => { 
+    modal.classList.add("hidden")
+    modalParagraph.innerText = "" 
+  }, 3000);
+}
+
+// hanlde the expression of the heart being liked/unliked
+let handleResponse = (event) => {
+  if (event.target.textContent === EMPTY_HEART){
+    event.target.classList.add("activated-heart")
+    event.target.textContent = FULL_HEART
+  }
+  else {
+    event.target.classList.remove("activated-heart")
+    event.target.textContent = EMPTY_HEART
+  }
+}
+
+// add an event listener over each element (each heartNode) of the array 
+heartsNodeArray.map(heartNode => {
+  heartNode.addEventListener('click', callServerAndCatch)
+})
+
+
+
+
+
 
 
 
@@ -23,3 +70,32 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
+/*
+
+
+  // majlind method
+  document.addEventListener("DOMContentLoaded", () => {
+    
+  
+    // select the class of like-glyph ("the heart")
+    const heart = document.querySelectorAll("span.like-glyph")
+    // console.log(heart)
+  
+    //   // need an event-handler, a click-event that reviews the like-glyph class
+  //   addEventListener('click', )
+  
+  // })
+  
+  //   // write a function heartLiker 
+  //   function heartLiker(){
+  //     console.log(heart.target)
+  //     mimicServerCall()
+  //     .then(() =>)
+  //   }
+  
+  })
+  
+  */
